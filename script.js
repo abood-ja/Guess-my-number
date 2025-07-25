6e928e156a44ddf366c9dc4cd95262870e79173e
abood.jarrar <abd.a.jarrar@gmail.com>
2025-07-25 13:02:36 +0300

    refactor: improve code structure and readability

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 'use strict';
 
-let secNumber = Math.trunc(Math.random() * 20) + 1;
+//selecting elements from the html
 const message = document.querySelector('.message');
-let score = 20;
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
+//initial the random number
+let secNumber = Math.trunc(Math.random() * 20) + 1;
+//initial the score to be 20
+let score = 20;
+//initial the state to be null
 let state = '';
+//initial high score with 0
 let highScore = 0;
-
+// function for styling the different states
+const styling = function (curState) {
+  if (curState === 'win') {
+    document.querySelector('.number').textContent = secNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else {
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    message.textContent = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+  }
+};
+// function for handling the again state
 const again = function () {
   state = '';
   secNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('body').style.backgroundColor = '#222';
-  message.textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.guess').value = '';
-  document.querySelector('.number').textContent = '?';
+  styling(state);
 };
-againBtn.addEventListener('click', again);
-checkBtn.addEventListener('click', function () {
-  if (state !== 'win') {
+//function for handling the check button
+const check = function () {
+  if (state === '') {
     const guess = Number(document.querySelector('.guess').value);
 
     if (!guess) {
@@ -33,11 +48,9 @@ checkBtn.addEventListener('click', function () {
       if (score > Number(document.querySelector('.highscore').textContent)) {
         document.querySelector('.highscore').textContent = score;
       }
-      document.querySelector('.number').textContent = secNumber;
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-      message.textContent = '🎉 Correct Number!';
       state = 'win';
+      styling(state);
+      message.textContent = '🎉 Correct Number!';
     }
     // when player lose
     else if (guess !== secNumber) {
@@ -45,21 +58,24 @@ checkBtn.addEventListener('click', function () {
         score--;
         document.querySelector('.score').textContent = score;
         if (guess > secNumber) {
-          if (guess - secNumber >= 5) {
-            message.textContent = '📈Too high ';
-          } else if (guess - secNumber < 5) {
-            message.textContent = 'High';
-          }
+          message.textContent = `${
+            guess - secNumber >= 5 ? '📈Too high' : 'High'
+          }`;
         } else if (guess < secNumber) {
-          if (secNumber - guess >= 5) {
-            message.textContent = '📉Too low';
-          } else if (secNumber - guess < 5) {
-            message.textContent = 'Low';
-          }
+          message.textContent = `${
+            secNumber - guess >= 5 ? '📉Too low' : 'Low'
+          }`;
         }
-      } else again();
+      } else {
+        again();
+      }
     }
   } else {
+    message.textContent = 'You lost!';
+    state = 'lose';
     again();
   }
-});
+};
+// using the two handlers functions
+againBtn.addEventListener('click', again);
+checkBtn.addEventListener('click', check);
